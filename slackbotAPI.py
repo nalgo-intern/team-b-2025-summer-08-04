af390ba094143cf057d6176e33947c11ef29a521
Yamazaki Sota <yamazaki.sota.intern@gmail.com>
2025-08-05 16:47:27 +0900

    slack上にコメントを送るコードの実装

--- slackbotAPI.py
+++ slackbotAPI.py
@@ -15,6 +15,41 @@ app = App(token=SLACK_BOT_TOKEN)
 #json形式のメッセージを溜めるためのリスト
 message_baffer = []
 
+def chack_data(llm_data:dict):
+    """LLMから受け取ったデータが条件を満たしているのか確認し
+    満たしているときにslackに送るtxtを作成する関数"""
+    
+    date = llm_data["date"]
+    start_time = llm_data["start_time"]
+    end_time = llm_data["end_time"]
+    summary = llm_data["summary"]
+    send_txt = ""
+    data_count = 0
+    chack = True # LLMから送られてきたデータが正しいか判定する変数
+    
+    if date != None:
+        data_count += 1
+        send_txt += f"日時：{date}\n"
+        
+    if start_time != None:
+        data_count += 1
+        send_txt += f"開始時間：{start_time}\n"
+    
+    if end_time != None:
+        send_txt += f"終了時間：{end_time}\n"
+        
+    if summary != None:
+        data_count += 1
+        send_txt += f"内容：{summary}\n"
+    
+    if data_count < 3:
+        chack = False
+        
+    return chack, send_txt
+        
+        
+    
+
 # メッセージ受信イベント（プライベートチャンネル対応）
 @app.event("message")
 def handle_message_events(body, say, logger):
@@ -45,9 +80,13 @@ def handle_message_events(body, say, logger):
     #バッファの長さを参照し、長さが10以上なら出力し、バッファリセット
     #このコードでは、say(text=f"メッセージ内容:{message_baffer}")　でslackのチャンネルにメッセージ内容を送信しているため、この部分をgeminiに送信するコードに変更してください。
     if len(message_baffer) >= 10:
-        say(text=f"メッセージ内容:{message_baffer}")
+        llm_data = llmからもらうdataの関数()
+        chack, send_txt = chack_data(llm_data)
+        if chack:
+            say(text=send_txt)
         message_baffer.clear()
-
+        
+        
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     print("インスタンス生成完了")
