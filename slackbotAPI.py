344678028d3b3eb5205e1b937e016b6474e4457b
Nakamura Ko <nakamura.ko.nalgo.intern@gmail.com>
2025-08-06 14:27:41 +0900

    botがメッセージに対してあらかじめスタンプを押すように改良

--- slackbotAPI.py
+++ slackbotAPI.py
@@ -19,6 +19,45 @@ message_baffer = []
 #起動時にBotのユーザーIDを取得
 bot_user_id = app.client.auth_test()["user_id"]
 
+def chack_data(llm_data:dict):
+    """LLMから受け取ったデータが条件を満たしているのか確認し
+    満たしているときにslackに送るtxtを作成する関数"""
+    
+    date = llm_data["date"]
+    start_time = llm_data["start_time"]
+    end_time = llm_data["end_time"]
+    summary = llm_data["summary"]
+    send_txt = ""
+    data_chack = [0, 0, 0] # どのデータがあるかを確認するためのlist(dateがある場合:data_chack[0]=1, start_timeがある場合:data_chack[1]=1, summaryがある場合:data_chack[2]=1)
+    chack = True # LLMから送られてきたデータが正しいか判定する変数
+    
+    if date != "None":
+        data_chack[0] = 1
+        send_txt += f"日程：{date}\n"
+    if start_time != "None":
+        data_chack[1] = 1
+        send_txt += f"開始時間：{start_time}\n"
+    if end_time != "None":
+        send_txt += f"終了時間：{end_time}\n"
+    if summary != "None":
+        data_chack[2] = 1
+        send_txt += f"予定内容：{summary}\n"
+        send_txt += f"これでよろしければスタンプ( :+1: もしくは :o: )を押してください"
+        
+    if sum(data_chack) == 2:
+        if not data_chack[0]:
+            send_txt = f"「日程」の情報を追加してください"
+        elif not data_chack[1]:
+            send_txt = f"「開始時間」の情報を追加してください"
+        else:
+            send_txt = f"「予定内容」の情報を追加してください"
+
+    if sum(data_chack) < 2:
+        chack = False
+            
+    return chack, send_txt
+
+
 # メッセージ受信イベント（プライベートチャンネル対応）
 @app.event("message")
 def handle_message_events(body, say, logger):
@@ -32,6 +71,12 @@ def handle_message_events(body, say, logger):
     user_id = event.get("user")
     text = event.get("text")
     
+    #イベントが発生したチャンネルIDの取得
+    channel_id = event["channel"]
+
+    # print("channel:",channel_id)
+    # print("ts:",ts)
+
     #ユーザーIDとメッセージ内容の取得でランタイムエラーが起きた場合
     if not user_id or not text:
         return
@@ -49,7 +94,22 @@ def handle_message_events(body, say, logger):
     #バッファの長さを参照し、長さが10以上なら出力し、バッファリセット
     #このコードでは、say(text=f"メッセージ内容:{message_baffer}")　でslackのチャンネルにメッセージ内容を送信しているため、この部分をgeminiに送信するコードに変更してください。
     if len(message_baffer) >= 10:
-        say(text=f"メッセージ内容:{message_baffer}")
+        #say(text=f"メッセージ内容:{message_baffer}")
+        llm_data= {"date": "2024-07-20", "start_time": "15:00", "end_time": "None","summary": "会議"}
+        check, send_txt = chack_data(llm_data)
+        
+        if check:
+            response = app.client.chat_postMessage(
+                channel=channel_id,
+                text=send_txt
+                )
+            target_ts = response["ts"]
+            for emoji in ["+1","o"]:
+                app.client.reactions_add(
+                    channel=channel_id,
+                    name=emoji,
+                    timestamp=target_ts
+                )
         message_baffer.clear()
 
 #リアクションされたときの処理(リアクションユーザー判断、メールアドレス取得)
@@ -59,8 +119,10 @@ def handle_reaction(event,say,logger):
     reaction = event.get("reaction")          # 使用されたリアクション
     item_user = event.get("item_user")        # リアクション対象メッセージの送信者
     
-    print("reaction:",reaction)
-    print("type:",type(reaction))
+    #botが自分でリアクションをつけた場合を除外
+    if user_id == bot_user_id:
+        return
+
     #Botが送ったメッセージに対するリアクションかを確認
     if item_user == bot_user_id:
         if reaction in ["o","+1"]: #特定のリアクションのみ対象にする
