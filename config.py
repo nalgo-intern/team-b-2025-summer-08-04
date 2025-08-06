888a12d7694f1ac1dcfa568d30dac0acf2a944f5
Yamazaki Sota <yamazaki.sota.intern@gmail.com>
2025-08-06 13:42:21 +0900

    ando branchからの取得

---
+++ config.py
@@ -0,0 +1,5 @@
+path_gemini_api_key = "AIzaSyCeoPw7uJliuxLVlzwNHI5mWRAEaxtONIo"
+path_slack_history = "sample_history.json"
+path_extracted_data = "extract.json"
+path_prompt_format = "prompt_format.txt"
+path_gemini_response = "gemini_response.json"
